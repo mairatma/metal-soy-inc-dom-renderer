e744bdfcc417f82bfe157bdfcc0b9d98ec8aea44
Maira Bello <maira.araujo@liferay.com>
2016-03-10 14:49:05 -0300

    Adds initial structure

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var metal = require('gulp-metal');
+
+metal.registerTasks({
+	bundleCssFileName: 'soyIncDomRenderer.css',
+	bundleFileName: 'soyIncDomRenderer.js',
+	moduleName: 'metal-soyIncDomRenderer'
+});
---
+++ test/SoyIncDomRenderer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import SoyIncDomRenderer from '../src/SoyIncDomRenderer';
+
+describe('SoyIncDomRenderer', function() {
+	it('should be tested', function() {
+		assert.fail('No tests for this module yet.');
+	});
+});
