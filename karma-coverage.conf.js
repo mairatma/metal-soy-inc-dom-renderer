fc8a2b9df0f6e6aa9f7949566f27c15463a39b1e
Maira Bello <maira.araujo@liferay.com>
2016-03-10 14:52:57 -0300

    Extends from IncrementalDomRenderer and makes sure the test file works

---
+++ karma-coverage.conf.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var metalKarmaConfig = require('metal-karma-config/coverage');
+
+module.exports = function (config) {
+  metalKarmaConfig(config);
+  config.files = [
+    'node_modules/metal-soy-incremental-dom-bundle/build/bundle.js',
+    'node_modules/metal*/src/**/*.js',
+    'src/**/*.js',
+    'test/**/*.js'
+  ];
+};
--- src/SoyIncDomRenderer.js
+++ src/SoyIncDomRenderer.js
@@ -1,6 +1,8 @@
 'use strict';
 
-class SoyIncDomRenderer {
+import IncrementalDomRenderer from 'metal-incremental-dom';
+
+class SoyIncDomRenderer extends IncrementalDomRenderer {
 }
 
 export default SoyIncDomRenderer;
