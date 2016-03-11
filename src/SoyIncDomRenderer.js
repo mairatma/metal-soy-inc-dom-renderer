23a639d4cbefba49c90d4a2aaeb218a4787fdcc8
Maira Bello <maira.araujo@liferay.com>
2016-03-11 15:01:29 -0300

    Implements rendering from the main template

--- src/SoyIncDomRenderer.js
+++ src/SoyIncDomRenderer.js
@@ -1,8 +1,42 @@
 'use strict';
 
+import core from 'metal';
 import IncrementalDomRenderer from 'metal-incremental-dom';
+import { SoyTemplates } from 'metal-soy';
 
 class SoyIncDomRenderer extends IncrementalDomRenderer {
+	/**
+	 * Copies the component's attributes to an object so it can be passed as it's
+	 * template call's data. The copying needs to be done because, if the component
+	 * itself is passed directly, some problems occur when soy tries to merge it
+	 * with other data, due to property getters and setters. This is safer.
+	 * @return {!Object}
+	 * @protected
+	 */
+	buildTemplateData_() {
+		var component = this.component_;
+		var data = {};
+		component.getAttrNames().forEach(name => {
+			// Get all attribute values except "element", since it helps performance
+			// and this attribute shouldn't be referenced inside a soy template anyway.
+			if (name !== 'element') {
+				data[name] = component[name];
+			}
+		});
+		return data;
+	}
+
+	/**
+	 * Overrides the default method from `IncrementalDomRenderer` so the component's
+	 * soy template can be used for rendering.
+	 * @override
+	 */
+	renderIncDom() {
+		var elementTemplate = SoyTemplates.get(this.component_.getName()).render;
+		if (core.isFunction(elementTemplate)) {
+			elementTemplate(this.buildTemplateData_());
+		}
+	}
 }
 
 export default SoyIncDomRenderer;
--- test/SoyIncDomRenderer.js
+++ test/SoyIncDomRenderer.js
@@ -1,11 +1,32 @@
 'use strict';
 
+import HelloWorldComponent from './assets/HelloWorld.soy';
 import SoyIncDomRenderer from '../src/SoyIncDomRenderer';
 
-import './assets/simple.soy';
+HelloWorldComponent.ATTRS = {
+	name: {}
+};
 
 describe('SoyIncDomRenderer', function() {
-	it('should be tested', function() {
-		assert.fail('No tests for this module yet.');
+	var comp;
+
+	afterEach(function() {
+		if (comp) {
+			comp.dispose();
+		}
+	});
+
+	it('should render component\'s "render" template', function() {
+		comp = new HelloWorldComponent().render();
+		assert.strictEqual('SPAN', comp.element.tagName);
+		assert.strictEqual('Hello World!', comp.element.textContent);
+	});
+
+	it('should pass attribute values to "render template"', function() {
+		comp = new HelloWorldComponent({
+			name: 'Foo'
+		}).render();
+		assert.strictEqual('SPAN', comp.element.tagName);
+		assert.strictEqual('Hello Foo!', comp.element.textContent);
 	});
 });
